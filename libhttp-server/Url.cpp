da742e53b5a6923950e8d0fdfb49d20c8f20f332
bjtj10 <bjtj10@gmail.com>
2015-10-19 14:08:22 +0900

    url string parsing added

--- libhttp-server/Url.cpp
+++ libhttp-server/Url.cpp
@@ -2,16 +2,24 @@
 
 #include "Url.hpp"
 
-namespace URL {
+namespace HTTP {
 
 	using namespace std;
-	
-	Url::Url() {
+
+	Url::Url(const char * urlStr) {
+		parseUrlString(urlStr);
 	}
+	Url::Url(const string & urlStr) {
+		parseUrlString(urlStr);
+	}
+	
 	Url::~Url() {
 	}
 
 	string Url::getScheme() {
+		return scheme;
+	}
+	string Url::getProtocol() {
 		return scheme;
 	}
 	string Url::getHost() {
@@ -20,10 +28,16 @@ namespace URL {
 	string Url::getPort() {
 		return port;
 	}
+	int Url::getIntegerPort() {
+		return UTIL::Text::toInt(port);
+	}
 	string Url::getPath() {
 		return path;
 	}
 	void Url::setScheme(string scheme) {
+		this->scheme = scheme;
+	}
+	void Url::setProtocol(string scheme) {
 		this->scheme = scheme;
 	}
 	void Url::setHost(string host) {
@@ -44,6 +58,49 @@ namespace URL {
 		return params;
 	}
 
+	void Url::parseUrlString(string urlStr) {
+
+		clear();
+		
+		size_t f = urlStr.find("://");
+		if (f == string::npos) {
+			throw -1;
+		}
+		scheme = urlStr.substr(0, f);
+		urlStr = urlStr.substr(f + 3);
+		f = urlStr.find("/");
+		if (f != string::npos) {
+			vector<string> addr = parseAddress(urlStr.substr(0, f));
+			setAddress(addr);
+			path = urlStr.substr(f);
+		} else {
+			vector<string> addr = parseAddress(urlStr);
+			setAddress(addr);
+			path = "/";
+		}
+	}
+
+	vector<string> Url::parseAddress(string address) {
+		vector<string> ret;
+		size_t f = address.find(":");
+		if (f != string::npos) {
+			ret.push_back(address.substr(0, f));
+			ret.push_back(address.substr(f + 1));
+		} else {
+			ret.push_back(address);
+		}
+		return ret;
+	}
+
+	void Url::setAddress(vector<string> & addr) {
+		host = addr[0];
+		if (addr.size() > 1) {
+			port = addr[1];
+		} else {
+			port = "80";
+		}
+	}
+
 	string Url::toString() {
 		string p;
 		p = UTIL::Text::toMapString(params, "=", "&");
@@ -52,6 +109,24 @@ namespace URL {
 		}
 
 		scheme + "://" + host + "/" + path + p;
+	}
+
+	Url& Url::operator=(const char * urlStr) {
+		this->parseUrlString(urlStr);
+		return *this;
+	}
+
+	Url& Url::operator=(const string & urlStr) {
+		this->parseUrlString(urlStr);
+		return *this;
+	}
+
+	void Url::clear() {
+		scheme = "";
+		host = "";
+		port = "";
+		path = "";
+		params.clear();
 	}
 
 }
--- libhttp-server/Url.hpp
+++ libhttp-server/Url.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-namespace URL {
+namespace HTTP {
 
 	class Url {
 	private:
@@ -13,16 +13,20 @@ namespace URL {
 		std::string port;
 		std::string path;
 		std::vector<std::pair<std::string, std::string> > params;
-		
+
 	public:
-		Url();
+		Url(const char * urlStr);
+		Url(const std::string & urlStr);
 		virtual ~Url();
 
 		std::string getScheme();
+		std::string getProtocol();
 		std::string getHost();
 		std::string getPort();
+		int getIntegerPort();
 		std::string getPath();
 		void setScheme(std::string scheme);
+		void setProtocol(std::string scheme);
 		void setHost(std::string host);
 		void setPort(std::string port);
 		void setPath(std::string path);
@@ -30,20 +34,18 @@ namespace URL {
 		void appendParam(std::string name, std::string value);
 		std::vector<std::pair<std::string, std::string> > & getParams();
 
+		void parseUrlString(std::string urlStr);
+		std::vector<std::string> parseAddress(std::string address);
+		void setAddress(std::vector<std::string> & addr);
+
 		virtual std::string toString();
+
+		Url& operator=(const std::string & urlStr);
+		Url& operator=(const char * urlStr);
+
+	protected:
+		void clear();
 	};
-
-
-	class UrlParser
-	{
-	public:
-		UrlParser();
-		virtual ~UrlParser();
-
-		virtual Url parse(std::string url);
-	};
-
-	
 }
 
 #endif
