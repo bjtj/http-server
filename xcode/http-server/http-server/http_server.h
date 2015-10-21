7310bbad5a571edeff505f6df1e6ed022b51d536
bjtj <bjtj10@gmail.com>
2015-10-21 23:27:45 +0900

    Xcode project added
    oslayer project needed

---
+++ xcode/http-server/http-server/http_server.h
@@ -0,0 +1,13 @@
+//
+//  http_server.h
+//  http-server
+//
+//  Created by TJ on 2015. 10. 21..
+//  Copyright © 2015년 TJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface http_server : NSObject
+
+@end
---
+++ xcode/http-server/http-server/http_server.mm
@@ -0,0 +1,13 @@
+//
+//  http_server.m
+//  http-server
+//
+//  Created by TJ on 2015. 10. 21..
+//  Copyright © 2015년 TJ. All rights reserved.
+//
+
+#import "http_server.h"
+
+@implementation http_server
+
+@end
