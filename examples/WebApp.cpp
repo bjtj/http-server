b1436eda8c25c359328ab7203b13079605f85978
bjtj <bjtj10@gmail.com>
2016-03-03 11:23:18 +0900

    prepare to implement web app solution

---
+++ examples/WebApp.cpp
@@ -0,0 +1,162 @@
+#include <liboslayer/os.hpp>
+#include <liboslayer/SecureSocket.hpp>
+#include <liboslayer/Utils.hpp>
+#include <liboslayer/Properties.hpp>
+#include <libhttp-server/AnotherHttpServer.hpp>
+#include <libhttp-server/FileTransfer.hpp>
+#include <libhttp-server/HttpEncoderDecoder.hpp>
+#include <libhttp-server/HttpSessionManager.hpp>
+#include <libhttp-server/HttpSessionTool.hpp>
+#include <libhttp-server/LispPage.hpp>
+#include <libhttp-server/Url.hpp>
+#include <liboslayer/Lisp.hpp>
+
+using namespace std;
+using namespace OS;
+using namespace UTIL;
+using namespace HTTP;
+
+const Logger & logger = LoggerFactory::getDefaultLogger();
+HttpSessionManager sessionManager(30 * 60 * 1000);
+
+class ServerConfig : public HttpServerConfig {
+private:
+    string host;
+    int port;
+    
+public:
+    
+    ServerConfig() : port(0) {}
+    virtual ~ServerConfig() {}
+    void load(const string & configPath) {
+        loadFromFile(configPath);
+        host = getProperty("domain.host");
+        port = getIntegerProperty("listen.port");
+    }
+    string getHost() { return host; }
+    int getPort() { return port; }
+};
+
+ServerConfig config;
+
+static void redirect(ServerConfig & config, HttpRequest & request, HttpResponse & response, HttpSession & session, const string & uri) {
+    
+    HttpResponseHeader & header = response.getHeader();
+    
+    string host = config.getHost();
+    string port = Text::toString(request.getLocalAddress().getPort());
+    if (!request.getHeaderField("Host").empty()) {
+        Url url("http://" + request.getHeaderField("Host"));
+        host = url.getHost();
+        port = url.getPort();
+    }
+    
+    response.setStatusCode(302);
+    header.setHeaderField("Location", "http://" +
+                          host + ":" + port + "/" +
+                          HttpSessionTool::urlMan(uri, session));
+    response.setContentType("text/html");
+    header.setConnection("close");
+}
+        
+class SinglePageHttpRequestHandler : public HttpRequestHandler {
+private:
+	string path;
+public:
+	SinglePageHttpRequestHandler(const string & path) : path(path) {}
+	virtual ~SinglePageHttpRequestHandler() {}
+
+	virtual void onHttpRequestHeaderCompleted(HttpRequest & request, HttpResponse & response) {
+
+		HttpSession & session = HttpSessionTool::getSession(sessionManager, request);
+		session.updateLastAccessTime();
+		
+		File file(path);
+		FileReader reader(file);
+
+		response.setStatusCode(200);
+		response.setContentType("text/html");
+            
+		LispPage page;
+		page.applyWeb();
+		page.applySession(session);
+		page.applyRequest(request);
+		page.applyResponse(response);
+		string content = page.parseLispPage(reader.dumpAsString());
+		
+		setFixedTransfer(response, content);
+	}
+};
+
+size_t readline(char * buffer, size_t max) {
+	if (fgets(buffer, (int)max - 1, stdin)) {
+		buffer[strlen(buffer) - 1] = 0;
+		return strlen(buffer);
+	}
+	return 0;
+}
+
+string prompt(const char * msg) {
+    char buffer[1024] = {0,};
+    printf("%s", msg);
+    if (readline(buffer, sizeof(buffer)) > 0) {
+        return string(buffer);
+    }
+    return "";
+}
+int promptInteger(const char * msg) {
+    string ret = prompt(msg);
+    return Text::toInt(ret);
+}
+bool promptBoolean(const char * msg) {
+    string ret = prompt(msg);
+    if (ret.compare("yes") || ret.compare("y")) {
+        return true;
+    }
+    return false;
+}
+
+
+int main(int argc, char * args[]) {
+	
+	if (argc > 1) {
+		config.load(args[1]);
+    } else {
+        char buffer[1024] = {0,};
+        printf("Configuration file path: ");
+        readline(buffer, sizeof(buffer));
+        config.load(buffer);
+    }
+    
+	System::getInstance()->ignoreSigpipe();
+
+    AnotherHttpServer * server = NULL;
+
+	config.setProperty("thread.count", 20);
+	server = new AnotherHttpServer(config);
+
+	SinglePageHttpRequestHandler single(config["default.page"]);
+	server->registerRequestHandler("/", &single);
+	server->registerRequestHandler("/index.htm*", &single);
+
+    printf("Listening... %d\n", config.getPort());
+    
+	server->startAsync();
+
+	while (1) {
+		char buffer[1024] = {0,};
+		if (readline(buffer, sizeof(buffer)) > 0) {
+			if (!strcmp(buffer, "q")) {
+				break;
+			} else if (!strcmp(buffer, "s")) {
+				printf("Listen port: %d\n", config.getPort());
+			}
+		}
+	}
+
+	server->stop();
+    delete server;
+    
+    return 0;
+}
+
